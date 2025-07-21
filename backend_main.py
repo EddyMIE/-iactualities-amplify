ac5699d02d6f56b7d10cb51bdf3723076d0f284e
EddyMIE <eddy.missoni@mc2i.fr>
2025-07-21 02:15:09 +0200

    Fix: Renommer variables AWS pour Amplify

--- backend_main.py
+++ backend_main.py
@@ -85,9 +85,9 @@ def get_bedrock_client():
     """Initialise le client AWS Bedrock"""
     return boto3.client(
         "bedrock-runtime",
-        region_name=os.getenv("AWS_REGION", "eu-west-3"),
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        region_name=os.getenv("CUSTOM_AWS_REGION", "eu-west-3"),
+        aws_access_key_id=os.getenv("CUSTOM_AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("CUSTOM_AWS_SECRET_ACCESS_KEY")
     )
 
 def get_azure_client(model_name: str):
